4a7610a6a86cdcfd1f6eeafbcdc75adccf9e2298
Joseane-Guedes <joseane_23@hotmail.com>
2021-09-30 21:17:50 -0300

    Add Lodash modular utilities

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,6 +1,10 @@
 import { memo, useState } from "react";
 import { AddProductToWishlistProps} from "./AddProductToWishlist";
 import dynamic from 'next/dynamic'
+import lodash from 'lodash';
+
+
+
 
 const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
   // return import('./AddProductToWishlist') Quando é export default
@@ -43,7 +47,7 @@ export function ProductItemComponent({
 export const ProductItem = memo(
   ProductItemComponent,
   (prevProps, nextProps) => {
-    return Object.is(prevProps.product, nextProps.product);
+    return lodash.isEqual(prevProps.product, nextProps.product);
   }
 );
 
